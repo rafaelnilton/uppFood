61e92357ac93739e1b9d458d45ea9f7abf8ba34c
PC-02\Rafael Nilton <rafael@upplify.com>
2016-11-03 12:15:37 -0300

    website

---
+++ js/app.js
@@ -0,0 +1,49 @@
+(function() {
+	'use strict';
+    var app = angular
+      .module('foodApp', [
+        'ui.router',
+        'foodControllers'
+      ])
+
+    .config(function($stateProvider, $urlRouterProvider) {
+        
+        
+      // For any unmatched url, redirect to /state1
+      $urlRouterProvider.otherwise("/home");
+        
+        
+        // Now set up the states
+        $stateProvider
+        .state('home', {
+          url: "/",
+          templateUrl: "views/default.html"
+        })
+        .state('chinese', {
+          url: "/chinese",
+          templateUrl: "views/chinese.html"
+        })
+        .state('burgers', {
+          url: "/burgers",
+          templateUrl: "views/burgers.html"
+        })
+        .state('salad', {
+          url: "/salad",
+          templateUrl: "views/salad.html"
+        })
+        
+        .state('cake', {
+          url: "/cake",
+          templateUrl: "views/cake.html"
+        })
+        
+        .state('pizza', {
+          url: "/pizza",
+          templateUrl: "views/pizza.html"
+        });
+        
+        
+     
+    });
+    
+})();
